9b16cd14f12d589fd2ec467344e26798c2625746
neojero <jboebion@gmail.com>
2024-01-29 11:40:54 +0100

    ajout workflow build maven

--- src/test/java/fr/afpa/pompey/cda/api/PersonControllerIntegrationTest.java
+++ src/test/java/fr/afpa/pompey/cda/api/PersonControllerIntegrationTest.java
@@ -22,6 +22,6 @@ public class PersonControllerIntegrationTest {
     public void getPersonsTest() throws Exception {
         mockMvc.perform(get("/persons"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].firstname", is("jero")));
+                .andExpect(jsonPath("$[0].firstname", is("John")));
     }
 }
