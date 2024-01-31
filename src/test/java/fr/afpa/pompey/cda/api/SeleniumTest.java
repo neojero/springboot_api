33920139edf4ef3ea809d8323038b66b866476c5
neojero <jboebion@gmail.com>
2024-01-31 16:54:01 +0100

    selenium ajout

--- src/test/java/fr/afpa/pompey/cda/api/SeleniumTest.java
+++ src/test/java/fr/afpa/pompey/cda/api/SeleniumTest.java
@@ -1,2 +1,15 @@
-package fr.afpa.pompey.cda.api;public class SeleniumTest {
+package fr.afpa.pompey.cda.api;
+
+import fr.afpa.pompey.cda.api.config.SeleniumConfig;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SeleniumTest {
+   private SeleniumConfig driver;
+    @Test
+    public void indexPageTest() {
+
+    }
 }
